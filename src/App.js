e7c1c43de42e10b956e05307fa4f0935360b0c68
Timur Ermolaev <timkogas@gmail.com>
2022-05-12 13:25:40 +0300

    #1 реализовал кнопку, которая отвечает за паузу и воспроизвидение

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import {useRef, useState} from 'react'
 import './App.css';
 
 function App() {
+  const [trackPlayed, setTrackPlayed] = useState(false);
+
+  const playAndPause = () => {
+    if (!trackPlayed) {
+      audioRef.current.play()
+      setTrackPlayed(prevState=>{
+        return !prevState
+      })
+    } else {
+      audioRef.current.pause()
+      setTrackPlayed(prevState=>{
+        return !prevState
+      })
+    }
+  }
+  const audioRef = useRef()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <audio controls ref={audioRef}>
+        <source src="./music/Rick_Astley_-_Never_Gonna_Give_You_Up_47958276.mp3" type='audio/mpeg'>
+        </source>
+      </audio>
+      <button onClick={playAndPause}>{trackPlayed ? "Pause" : 'Play'}</button>
     </div>
   );
 }
